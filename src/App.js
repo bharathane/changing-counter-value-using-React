6986d4e911a9077758a95df948bafff3dec9b8f5
bharathane <mandadurgabharath@gmail.com>
2023-10-22 07:08:20 +0000

    commit message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import ClickCounter from './components/ClickCounter'
+import {Component} from 'react'
+import AllSeats from './components/AllSeats'
+import SeatsContext from './seatsContext/context'
 
-import './App.css'
+class App extends Component {
+  state = {btnsIds: []}
 
-const App = () => <ClickCounter />
+  setSeatIds = ids => {
+    this.setState(prevState => ({btnsIds: [...prevState.btnsIds, ...ids]}))
+  }
+
+  render() {
+    const {btnsIds} = this.state
+    console.log(btnsIds)
+    return (
+      <SeatsContext.Provider
+        value={{seatIds: btnsIds, setSeatIds: this.setSeatIds}}
+      >
+        <AllSeats />
+      </SeatsContext.Provider>
+    )
+  }
+}
 
 export default App
